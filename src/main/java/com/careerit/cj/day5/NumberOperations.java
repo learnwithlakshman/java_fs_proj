e28da3c9a7594e20b7bdbe1880b7b1adef26b944
learnwithlakshman@gmail.com <learnwithlakshman@gmail.com>
2022-09-13 07:57:58 +0530

    Added while loop example

---
+++ src/main/java/com/careerit/cj/day5/NumberOperations.java
@@ -0,0 +1,11 @@
+package com.careerit.cj.day5;
+
+public class NumberOperations {
+
+        public static int reverse(int num){
+            return 0;
+        }
+        public static int sumOfDigits(int num){
+            return 0;
+        }
+}
